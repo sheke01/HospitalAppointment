36609d608a0ee754455bfd66da2185be3dfd0cdc
Ekeh Vincent <shekevincent01@gmail.com>
2020-11-21 09:57:11 -0500

    first commit

---
+++ HospitalAppointment/src/test/java/com/sheke/demo/HospitalAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheke.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
